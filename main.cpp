aedafe6138eeb6754644cbabc0a4b7b8d66d8eaf
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-11-04 00:14:43 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int snt_counter = 0; 
+    int wrd_counter = 0;
+    string stroka;
+    cout << "Введите предложение: " << endl;
+    getline(cin, stroka);
+
+    for (int i = 0; i < stroka.size(); i++){
+
+        if (stroka[i] == '.'){
+            if (stroka[i+1] == '.' and stroka[i+2] == '.'){
+                snt_counter++;
+                wrd_counter++;
+                i+=2;
+            }
+            else {
+                snt_counter++;
+                wrd_counter++;
+            }
+        }
+
+        if (stroka[i] == '!'){
+            snt_counter++;
+            wrd_counter++;
+        }      
+
+        if (stroka[i] == '?'){
+            snt_counter++;
+            wrd_counter++; 
+        }
+        
+        if (stroka[i] == ' ') {
+            if (stroka[i-1] != '!' and stroka[i-1] != '.' and stroka[i-1] != '?' and stroka[i-1] != ',' and stroka[i-1] != ';' and stroka[i-1] != ':' and stroka[i-1] != '-') {
+                if ((stroka[i+1] == ',' or stroka[i+1] == ';' or stroka[i+1] == ':' or stroka[i+1] == '-') and stroka[i+2] != ' ') {
+                    wrd_counter++;
+                    i++;}
+                else if ((stroka[i+1] == ',' or stroka[i+1] == ';' or stroka[i+1] == ':' or stroka[i+1] == '-') and stroka[i+2] == ' ') {
+                    wrd_counter++;
+                    i+=2;}
+                else wrd_counter++;
+                }
+            else i++;
+        }
+
+        if (stroka[i] == ',' or stroka[i] == ';' or stroka[i] == ':') wrd_counter++;
+
+        }
+
+    cout << "Количество предложений в строке = " << snt_counter << endl;
+    cout << "Количество слов в строке = " << wrd_counter << endl;
+}
